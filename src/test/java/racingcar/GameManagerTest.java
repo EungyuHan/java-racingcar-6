0583a0a275aa7e4e27044a5aeafa633ed323e22f
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-10-06 16:24:41 +0900

    feat #1-1 : 사용자의 입력에서 같은 이름을 가진 자동차 확인

--- src/test/java/racingcar/GameManagerTest.java
+++ src/test/java/racingcar/GameManagerTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
@@ -22,5 +23,16 @@ class GameManagerTest {
         for (int i = 0; i < cars.size(); i++) {
             assertThat(cars.get(i).getCarName()).isEqualTo(resultString[i]);
         }
+    }
+
+    @Test
+    void 중복된_이름_입력() {
+        String testString = "car1,car2,car1";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(testString.getBytes());
+        System.setIn(inputStream);
+
+        assertThatThrownBy(() -> gameManager.setParticipateCars())
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("같은 이름을 가진 자동차가 있습니다.");
     }
 }
